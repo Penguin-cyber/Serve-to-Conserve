ad0bb5de7bb44091293ff2c16b9ef2047deb587d
Penguin-cyber <mengyangyu2008@gmail.com>
2024-11-16 10:29:23 -0500

    CUBE

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-import * as thr from "three";
+import * as THREE from "./three.module.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -10,7 +10,8 @@ const camera = new THREE.PerspectiveCamera(
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setAnimationLoop(animate);
+document.getElementById("scene-container").appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -20,6 +21,8 @@ scene.add(cube);
 camera.position.z = 5;
 
 function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
   renderer.render(scene, camera);
 }
-renderer.setAnimationLoop(animate);
