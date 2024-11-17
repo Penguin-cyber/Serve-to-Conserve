dee4ca82a0dee6c6b26e4d909856afa8a2b8a99f
Penguin-cyber <mengyangyu2008@gmail.com>
2024-11-17 10:07:02 -0500

    t

--- static/js/pop up.js
+++ static/js/pop up.js
@@ -3,7 +3,8 @@ const unused_clothing_popup = document.getElementById("unused_clothing_popup");
 const unused_clothing_overlay = document.getElementById(
   "unused_clothing_overlay"
 );
-
+const unused_clothing_design = document.getElementById("design_button_popup");
+const unused_clothing_donate = document.getElementById("donate_button_popup");
 const button = document.getElementById("test_button");
 
 var check_unused_clothing = 0;
@@ -13,10 +14,14 @@ function unused_clothing() {
     check_unused_clothing = 1;
     unused_clothing_popup.style.display = "block";
     unused_clothing_overlay.style.display = "block";
+    unused_clothing_donate.style.display = "block";
+    unused_clothing_design.style.display = "block";
   } else {
     check_unused_clothing = 0;
     unused_clothing_popup.style.display = "none";
     unused_clothing_overlay.style.display = "none";
+    unused_clothing_donate.style.display = "none";
+    unused_clothing_design.style.display = "none";
   }
   console.log("HEllo world");
 }
