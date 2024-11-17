91aee70b3bc4399d0ee0d197266e7a0756a0418a
AldenStripes <114105823+AldenStripes@users.noreply.github.com>
2024-11-17 01:11:30 -0500

    counter

--- static/js/leaves.js
+++ static/js/leaves.js
@@ -1,1 +1,13 @@
 //js for managing leaves counter and log outfit popup
+var leaves = 0;
+const confirm_button = document.getElementById('confirm-button');
+document.getElementById('confirm-button').addEventListener("click", () => {
+    console.log('clicked confirm');
+    leaves += 5;
+    updateLeaves();
+});
+
+function updateLeaves() {
+    document.getElementById('leaves').innerHTML = leaves;
+}
+console.log('asiofjoas')
