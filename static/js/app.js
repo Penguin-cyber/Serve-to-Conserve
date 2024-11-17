07ec87e5a36173a4eeec2d4a27047fb22925a244
Penguin-cyber <mengyangyu2008@gmail.com>
2024-11-16 19:15:50 -0500

    t

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,23 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
+let model; // Declare a variable to store the model
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
+  (window.innerWidth / window.innerHeight) * 2,
   0.1,
   1000
 );
 
 const renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight / 2);
 renderer.setAnimationLoop(animate);
 document.getElementById("scene-container").appendChild(renderer.domElement);
 
-const spotLight = new THREE.SpotLight(0xffffff, 10, 100, Math.PI / 4, 0.1, 1); // white spotlight
-spotLight.position.set(0, 10, 10);
+const spotLight = new THREE.SpotLight(0xffffff, 5, 100, Math.PI / 4, 0.1, 0.4); // white spotlight
+spotLight.position.set(0, 10, 5);
 scene.add(spotLight);
 
 // Load GLTF model
@@ -23,12 +25,12 @@ const loader = new GLTFLoader();
 loader.load(
   "/static/3d models/animal crossing character/scene.gltf", // Replace with the path to your model
   (gltf) => {
-    const model = gltf.scene;
+    model = gltf.scene;
     scene.add(model);
 
     // Position and scale the model as needed
-    model.position.set(0, 0, 0);
-    model.scale.set(1, 1, 1);
+    model.position.set(0, -2, 0);
+    model.scale.set(1.5, 1.5, 1.5);
   },
   undefined,
   (error) => {
@@ -36,11 +38,18 @@ loader.load(
   }
 );
 
+camera.position.x = 0;
+camera.position.y = 2;
 camera.position.z = 5;
+camera.lookAt(0, 0, 0);
 
 // Animation loop
 function animate() {
-  requestAnimationFrame(animate);
+  // Rotate the model if it's loaded
+  if (model) {
+    model.rotation.y += 0.01; // Rotate around the y-axis
+  }
+
   renderer.render(scene, camera);
 }
 
