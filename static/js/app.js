78298de267f1b62cc66286671a79fdc0101ec8eb
Penguin-cyber <mengyangyu2008@gmail.com>
2024-11-17 13:24:22 -0500

    t

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,18 @@
 import * as THREE from "three";
-import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import WebGL from "three/addons/capabilities/WebGL.js";
 
 let model;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / (window.innerHeight / 2.5),
-    0.1,
-    1000
+  75,
+  window.innerWidth / (window.innerHeight / 2.5),
+  0.1,
+  1000
 );
 
-const renderer = new THREE.WebGLRenderer({alpha: true});
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight / 2.5);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setAnimationLoop(animate);
@@ -26,19 +26,19 @@ scene.add(spotLight);
 const loader = new GLTFLoader();
 
 loader.load(
-    "/static/3d models/animal crossing character/villager1.glb", // Replace with the path to your model
-    (gltf) => {
-        model = gltf.scene;
-        scene.add(model);
+  "/static/3d models/animal crossing character/villager1.glb", // Replace with the path to your model
+  (gltf) => {
+    model = gltf.scene;
+    scene.add(model);
 
-        // Position and scale the model as needed
-        model.position.set(0, -2, 0);
-        model.scale.set(5, 5, 5);
-    },
-    undefined,
-    (error) => {
-        console.error("An error occurred loading the model:", error);
-    }
+    // Position and scale the model as needed
+    model.position.set(-0.15, -2, 0);
+    model.scale.set(5, 5, 5);
+  },
+  undefined,
+  (error) => {
+    console.error("An error occurred loading the model:", error);
+  }
 );
 
 camera.position.x = 0;
@@ -48,30 +48,30 @@ camera.lookAt(0, 1.5, 0);
 
 // Animation loop
 function animate() {
-    // Rotate the model if it's loaded
-    if (model) {
-        model.rotation.y += 0.01; // Rotate around the y-axis
-    }
+  // Rotate the model if it's loaded
+  if (model) {
+    model.rotation.y += 0.01; // Rotate around the y-axis
+  }
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 function resize() {
-    renderer.setSize(window.innerWidth, window.innerHeight / 2.5);
-    renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight / 2.5);
+  renderer.setPixelRatio(window.devicePixelRatio);
 
-    camera.aspect = window.innerWidth / (window.innerHeight / 2.5);
-    camera.updateProjectionMatrix();
+  camera.aspect = window.innerWidth / (window.innerHeight / 2.5);
+  camera.updateProjectionMatrix();
 }
 
 function initMain() {
-    if (WebGL.isWebGLAvailable()) {
-        window.addEventListener("resize", () => resize());
-        animate();
-    } else {
-        const warning = WebGL.getWebGLErrorMessage();
-        document.getElementById("view").appendChild(warning);
-    }
+  if (WebGL.isWebGLAvailable()) {
+    window.addEventListener("resize", () => resize());
+    animate();
+  } else {
+    const warning = WebGL.getWebGLErrorMessage();
+    document.getElementById("view").appendChild(warning);
+  }
 }
 
 initMain();
